5031c4420d2bc7d94d08f3d247af64332ac86cc1
Jordan McRae <jordanm@newyyz.com>
2015-07-09 16:40:20 -0400

    Have proper json file loading up for message based on params, but having trouble displaying

--- app/scripts/directives/messagedirective.js
+++ app/scripts/directives/messagedirective.js
@@ -9,9 +9,21 @@
       templateUrl: "scripts/directives/message.tmpl.html",
       controllerAs: 'message',
 
-      controller: function ($routeParams, $scope, MessageFactory) {
+      controller: function ($routeParams, $scope, MessageFactory) { //$routeParams is passed to messageFactory for use in .get
+        this.message = {};
 
-      }
+
+        var getmessage = MessageFactory.getMessage($routeParams);
+
+        // if(getmessage) {
+        //   getmessage.then(angular.bind(this, function (response) {
+        //     MessageFactory.message = response;
+        //     this.message = MessageFactory.message;
+        //     $scope.$parent.message.subject = this.message.subject;
+        //   }));
+        // }
+
+      },
     }
 
   }));
--- app/scripts/factories/messagefactory.js
+++ app/scripts/factories/messagefactory.js
@@ -1,14 +1,14 @@
 (angular.module('messagesApp')
   .factory('MessageFactory', 
-    function MessageFactory ($q) { 
+    function MessageFactory ($q, $http) { 
 	'use strict';
 
 	var exports = {};
 
 		exports.getMessage = function(params) {
-			if (params.id) {
+			if (params.id) { //Checks to see if there is an id on the params
 				var deferred = $q.defer();
-				$http.get('json/message/' + params.id + '.json')
+				$http.get('json/messages/' + params.id + '.json') //Params is passed in via the directive through $routeParams (passes in an entire object, not just id)
 				.success(function(data) {
 					deferred.resolve(data);
 				})
